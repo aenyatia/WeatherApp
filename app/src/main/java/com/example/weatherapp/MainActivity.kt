e6ba384d27113b0505d94406e33b8e11c27696a9
Aenyatia Nirvalen <aenyatia@gmail.com>
2024-01-31 01:00:08 +0100

    Add pager with main screens.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,15 +1,23 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.example.weatherapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +25,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
@@ -30,17 +38,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun App() {
+    val pagerState = rememberPagerState { 3 }
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+    ) {
+        when (it) {
+            0 -> CurrentWeatherPage()
+            1 -> DetailsWeatherPage()
+            2 -> DailyWeatherPage()
+        }
+    }
+}
+
+@Composable
+fun CurrentWeatherPage() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Current Weather Page",
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
-    }
+fun DetailsWeatherPage() {
+    Text(
+        text = "Details Weather Page",
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun DailyWeatherPage() {
+    Text(
+        text = "Daily Weather Page",
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+    )
 }
